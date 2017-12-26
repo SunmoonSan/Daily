2604d6d415bea9a052aca53b2085f8de2cfda86f
San <in.sunmoon.san@gmail.com>
2017-12-27 00:09:42 +0800

    the use of 'strings'

---
+++ maple/__strings__.py
@@ -0,0 +1,11 @@
+
+name = "shenzhen"
+
+print(name.capitalize())
+print(name.count('h'))
+print(name.encode())
+print(name.endswith('n'))
+print(name.startswith('h'))
+print(name.center(50, '*'))
+print(name.find('h'))
+print('--'.join(name))
