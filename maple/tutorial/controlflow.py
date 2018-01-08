c37c5a151f7f1b337420ba54e6a8c48f53696d80
San <18814184179@163.com>
2018-01-08 22:27:23 +0800

    Python流程控制
    Signed-off-by: San <18814184179@163.com>

--- maple/tutorial/controlflow.py
+++ maple/tutorial/controlflow.py
@@ -127,3 +127,122 @@ def fib2(n):
 
 f300 = fib2(300)  # 有返回值
 print(f300)
+
+
+# 7.深入Python函数定义
+'''def ask_ok(prompt, retries=4, complaint='Yes or no, please!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise OSError('uncooperative user')
+        print(complaint)
+'''
+
+# ask_ok('h')
+
+i = 5
+
+
+def f(arg=i):
+    print(arg)
+
+
+i = 6
+f()
+
+
+def f1(a, L=[]):
+    L.append(a)
+    return L
+
+
+print(f1(1))  # [1]
+print(f1(2))  # [1, 2]
+print(f1(3))  # [1, 2, 3]
+
+
+def f2(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+
+print(f2(1))  # [1]
+print(f2(2))  # [1, 2]
+print(f2(3))  # [1, 2, 3]
+
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("--This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("--Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+
+parrot(1000)
+parrot(voltage=1000)
+parrot(voltage=1000000, action='V00000M')
+parrot(action='V00000M', voltage=1000000)
+parrot('a million', 'better of life', 'jump')
+parrot('a thousand', state='pushing up the daisies')
+
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-"*40)
+
+    keys = sorted(keywords.keys())
+    for kw in keys:
+        print(kw, ":", keywords[kw])
+
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
+
+
+print(list(range(3, 6)))
+args = [3, 6]
+print(list(range(*args)))
+
+
+def parrot2(voltage, state='a stiff', action='voom'):
+    print("--This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.", end=' ')
+    print("E's", state, "!")
+
+
+d = {"voltage": "four million", "state": "bleedin' demised", "action": "VOOM"}
+parrot(**d)
+
+
+def make_incrementor(n):
+    return lambda x: x+n
+
+
+f = make_incrementor(42)
+print(f(0))
+print(f(1))
+
+
+def my_function():
+    """
+    Do nothing, but document it.
+    No, really, it doesn't do anything.
+    :return:
+    """
+    pass
+
+
+print(my_function.__doc__)
