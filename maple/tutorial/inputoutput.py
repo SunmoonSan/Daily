97c2696741b30aefd20143adfe45ecf5bfacfad6
San <18814184179@163.com>
2018-01-12 23:58:01 +0800

    输入和输出
    Signed-off-by: San <18814184179@163.com>

--- maple/tutorial/inputoutput.py
+++ maple/tutorial/inputoutput.py
@@ -49,3 +49,11 @@ import math
 print('The value of PI is approximately {}.'.format(math.pi))
 print('The value of PI is approximately {!r}.'.format(math.pi))
 print('The value of PI is approximately {0:.3f}.'.format(math.pi))
+
+table = {'Sjoerd': 4127, 'Jack':4098, 'Dcab':7678}
+for name, phone in table.items():
+    print('{0:10} ==> {1:10d}'.format(name, phone))
+
+# print('Jack:{0[Jack]:d};Sjoerd:{0[Sjoerd]:d};')
+
+print('The value of PI is approximately %5.3f.' % math.pi)
