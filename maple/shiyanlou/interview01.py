ce8e0c03de4dd79be45d365c167815a6a34f1a98
San <18814184179@163.com>
2018-03-13 23:55:18 +0800

    类对象，继承
    Signed-off-by: San <18814184179@163.com>

---
+++ maple/shiyanlou/interview01.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @desc  : Created by San on 18-3-13 下午11:46
+# @site  : https://github.com/SunmoonSan
+
+
+class A(object):
+    def show(self):
+        print("base show")
+
+
+class B(A):
+    def show(self):
+        print("derived show")
+
+# 调用B类show()方法
+
+
+obj = B()
+obj.show()
+print(obj.__class__)
+
+# 调用A类show()方法
+obj.__class__ = A # 将A的类对象赋值给A
+print(obj.__class__)
+obj.show()
