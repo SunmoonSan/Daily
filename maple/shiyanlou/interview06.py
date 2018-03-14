af9a2ae82ac2f4463bb432c7f504b82a04f6e03c
San <18814184179@163.com>
2018-03-14 23:17:41 +0800

    交换变量
    Signed-off-by: San <18814184179@163.com>

---
+++ maple/shiyanlou/interview06.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @desc  : Created by San on 18-3-14 下午10:57
+# @site  : https://github.com/SunmoonSan
+
+a = 8
+b = 9
+
+
+a, b = b, a
+
+print(a)
+print(b)
