7fbf82754d19767dd51f7cac35a76e3186b27110
San <18814184179@163.com>
2018-06-15 14:56:47 +0800

    多进程
    Signed-off-by: San <18814184179@163.com>

---
+++ maple/corepython/chapter04/myThread.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @desc  : Created by San on 2018-06-15 11:36
+# @site  : https://github.com/SunmoonSan
+import threading
+from time import ctime
+
+
+class MyThread(threading.Thread):
+
+    def __init__(self, func, args, name=''):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.func = func
+        self.args = args
+
+    def getResult(self):
+        return self.res
+
+    def run(self):
+        print('starting', self.name, 'at: ', ctime())
+        self.res = self.func(*self.args)
+        print(self.name, 'finished at: ', ctime())
